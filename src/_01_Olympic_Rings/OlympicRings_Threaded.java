d9643c1a1264129cec31651324b9af9e4331bf93
league <league@wts14-3.attlocal.net>
2025-01-16 17:00:25 -0800

    Still working on Olympic Rings

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,58 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	
+	public static void main(String[] args) {
+		Robot r1 = new Robot(100, 100);
+		Robot r2 = new Robot(200, 100);
+		Robot r3 = new Robot(300, 100);
+		Robot r4 = new Robot(150, 200);
+		Robot r5 = new Robot(200, 200);
+		r1.setSpeed(10);
+		r2.setSpeed(10);
+		r3.setSpeed(10);
+		r4.setSpeed(10);
+		r5.setSpeed(10);
+		Thread m1 = new Thread(()-> {
+			for (int i = 0; i < 360; i++) {
+				r1.move(3);
+				r1.turn(1);
+			}
+			
+		});
+		Thread m2 = new Thread(()-> {
+			for (int i = 0; i < 360; i++) {
+				r2.move(3);
+				r2.turn(1);
+			}
+		});
+		Thread m3 = new Thread(()-> {
+			for (int i = 0; i < 360; i++) {
+				r3.move(3);
+				r3.turn(1);
+			}
+		});
+		Thread m4 = new Thread(()-> {
+			for (int i = 0; i < 360; i++) {
+				r4.move(3);
+				r4.turn(1);
+			}
+		});
+		Thread m5 = new Thread(()-> {
+			for (int i = 0; i < 360; i++) {
+				r5.move(3);
+				r5.turn(1);
+			}
+		});
+		m1.start();
+		m2.start();
+		m3.start();
+		m4.start();
+		m5.start();
+	}
+	
 }
 
